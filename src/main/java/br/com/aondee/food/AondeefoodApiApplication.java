522e8107fb920c2b41fd50f3f32a1ea5b4d1ed1e
Jean Paul <jeanpaulwebb@gmail.com>
2022-04-13 00:32:33 -0300

    feat: Configuring the initial project with spring boot

---
+++ src/main/java/br/com/aondee/food/AondeefoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.aondee.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AondeefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AondeefoodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aondee/food/AondeefoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aondee.food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AondeefoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
